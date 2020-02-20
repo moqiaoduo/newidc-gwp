9b14df95b4c1498c0a218522430a43afaebe1824
moqiaoduo <415372363@qq.com>
2020-02-20 22:47:43 +0800

    产品配置(初)

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,7 +12,11 @@ class Plugin extends Server
     public static function productConfig()
     {
         return [
-            ''
+            'storage'=>['type'=>'number','label'=>'储存容量','help'=>'单位MB','min'=>0],
+            'domain'=>['type'=>'number','label'=>'可绑定域名数','min'=>0,'help'=>'0为不限'],
+            'sub_domain'=>['type'=>'number','label'=>'可绑定子域名数','min'=>-1,'help'=>'-1为不限'],
+            'alias_domain'=>['type'=>'number','label'=>'可绑定别名数','min'=>-1,'help'=>'-1为不限'],
+            'traffic'=>['type'=>'number','限制流量','help'=>'单位MB','min'=>0]
         ];
     }
 
